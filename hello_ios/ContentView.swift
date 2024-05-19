6983dba6180ff0c9d39cec4032e22480683e4e6c
Md. Mahfuzur Rahman <mahfuzur.rahman@brainstation-23.com>
2024-05-19 13:14:17 +0600

    Initial iOS project

---
+++ hello_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello_ios
+//
+//  Created by BS1002 on 19/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello_ios/hello_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_iosApp.swift
+//  hello_ios
+//
+//  Created by BS1002 on 19/5/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
