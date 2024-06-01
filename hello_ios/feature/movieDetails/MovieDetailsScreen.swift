5ace248b35bc4a1af735dda427520d3371c513fa
Md. Mahfuzur Rahman <mahfuzur.rahman@brainstation-23.com>
2024-06-01 22:07:17 +0600

    Improved movie details screen

--- hello_ios/feature/movieDetails/MovieDetailsScreen.swift
+++ hello_ios/feature/movieDetails/MovieDetailsScreen.swift
@@ -5,6 +5,7 @@
 //  Created by BS1002 on 23/5/24.
 //
 
+
 import Foundation
 import SwiftUI
 
@@ -22,68 +23,11 @@ struct MovieDetailsScreen: View {
             if let movie = viewmodel.movieDetails {
                 ScrollView {
                     VStack(alignment: .leading, spacing: 16) {
-                        // Cover Image
-                        AsyncImage(url: URL(string: movie.largeCoverImage)) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            case .failure:
-                                Image(systemName: "xmark.octagon.fill")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.red)
-                            @unknown default:
-                                EmptyView()
-                            }
-                        }
-                        
-                        // Title and Release Year
-                        HStack {
-                            Text(movie.title)
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Spacer()
-                            Text("\(movie.releaseYear)")
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                        }
-                        
-                        // Rating
-                        HStack {
-                            ForEach(0..<5) { star in
-                                Image(systemName: star < Int(movie.rating.rounded()) ? "star.fill" : "star")
-                                    .foregroundColor(star < Int(movie.rating.rounded()) ? .yellow : .gray)
-                            }
-                            Text(String(format: "%.1f", movie.rating))
-                                .foregroundColor(.gray)
-                                .font(.subheadline)
-                        }
-                        
-                        // Genres
-                        HStack {
-                            ForEach(movie.genres.indices, id: \.self) { index in
-                                Text(movie.genres[index].toString())
-                                    .font(.caption)
-                                    .padding(.horizontal, 8)
-                                    .padding(.vertical, 4)
-                                    .background(Color.blue)
-                                    .cornerRadius(8)
-                                    .foregroundColor(.white)
-                            }
-                        }
-                        
-                        // Duration
-                        Text("Duration: \(movie.duration) min")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        
-                        // Description
-                        Text(movie.description)
-                            .font(.body)
+                        MovieCoverImageView(url: movie.largeCoverImage)
+                        MovieTitleAndYearView(title: movie.title, year: movie.releaseYear)
+                        MovieRatingView(rating: movie.rating)
+                        MovieGenresView(genres: movie.genres)
+                        MovieDurationAndDescriptionView(duration: movie.duration, description: movie.description)
                     }
                     .padding()
                 }
@@ -91,12 +35,104 @@ struct MovieDetailsScreen: View {
             } else {
                 ProgressView()
             }
-        }
-        .onAppear {
+        }.onAppear() {
             viewmodel.getMovieDetails(movieId: movieId)
         }
-        .onDisappear {
+        .onDisappear() {
             viewmodel.clearMovieDetails()
+        }
+    }
+}
+
+fileprivate struct MovieCoverImageView: View {
+    let url: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: url)) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            case .failure:
+                Image(systemName: "xmark.octagon.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.red)
+            @unknown default:
+                EmptyView()
+            }
+        }
+    }
+}
+
+fileprivate struct MovieTitleAndYearView: View {
+    let title: String
+    let year: Int
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+            Text("\(year)")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+fileprivate struct MovieRatingView: View {
+    let rating: Double
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<5) { star in
+                Image(systemName: star < Int(rating.rounded()) ? "star.fill" : "star")
+                    .foregroundColor(star < Int(rating.rounded()) ? .yellow : .gray)
+            }
+            Text(String(format: "%.1f", rating))
+                .foregroundColor(.gray)
+                .font(.subheadline)
+        }
+    }
+}
+
+fileprivate struct MovieGenresView: View {
+    let genres: [MovieGenre]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(genres.indices, id: \.self) { index in
+                    Text(genres[index].toString())
+                        .font(.caption)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 4)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct MovieDurationAndDescriptionView: View {
+    let duration: Int
+    let description: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Duration: \(duration) min")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text(description)
+                .font(.body)
         }
     }
 }
